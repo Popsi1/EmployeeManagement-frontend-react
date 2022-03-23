d258dee3571d212328e3804790fc4462c5cbf74b
Umeigbo Obumneme Steven <obumneme.umeigbo@decagon.dev>
2022-03-23 12:24:19 +0100

    employee management system

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import AddEmployeeComponent from './components/AddEmployeeComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className= "container">
+          <Switch>
+              <Route exact path = "/" component = {ListEmployeeComponent}></Route>
+              <Route path = "/employees" component = {ListEmployeeComponent}></Route>
+              <Route path = "/add-employee" component = {AddEmployeeComponent} ></Route>
+              <Route path = "/edit-employee/:id" component = {AddEmployeeComponent}></Route> 
+            </Switch>
+        </div>
+        <FooterComponent />
+        </Router>
     </div>
   );
 }
